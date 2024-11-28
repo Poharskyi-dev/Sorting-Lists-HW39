9ef8680a4fdb5c70b1e837b4ca03cb0250f752ff
Poharskyi <avpog.box@gmail.com>
2024-11-28 22:41:12 +0100

    add code

---
+++ src/main/java/com/gmail/box/avpog/utils/Student.java
@@ -0,0 +1,40 @@
+package com.gmail.box.avpog.utils;
+
+public class Student implements Comparable<Student> {
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(this.age, o.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
